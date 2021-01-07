8736a16191f50973dba959420b875a3ebdaf11eb
levkarma <levko.k9@gmail.com>
2021-01-07 11:44:19 -0500

    feat: frontend created

---
+++ src/components/AddCollaborator.vue
@@ -0,0 +1,70 @@
+<template>
+  <section class="section">
+    <b-message type="is-info">
+      Please enter your <strong>Github username</strong> to be immediately added
+      as a collaborator to the private repository
+      <strong>{{ repositoryPath }}</strong>
+    </b-message>
+    <b-field label="Your Github Username">
+      <b-input v-model="username"></b-input>
+    </b-field>
+    <b-field label="The Secret I Provided To You">
+      <b-input v-model="secret"></b-input>
+    </b-field>
+    <section class="mb-4">
+      <b-button @click="clickMe">Click Me</b-button>
+    </section>
+    <b-message type="is-success" v-if="success">
+      Successfully invited you to be a collaborator. Please visit
+      <strong
+        ><a :href="fullGithubRepoUrl">{{ fullGithubRepoUrl }}</a></strong
+      >
+      or check your
+      <strong>Github associated email</strong>
+      to accept the invitation.
+    </b-message>
+    <b-message type="is-danger" v-if="failure"
+      >Please ensure the username and secret are valid.</b-message
+    >
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      secret: "",
+      success: false,
+      failure: false,
+    };
+  },
+  computed: {
+    repositoryPath() {
+      return `${this.$route.params.owner}/${this.$route.params.repo}`;
+    },
+    fullGithubRepoUrl() {
+      return `https://github.com/${this.$route.params.owner}/${this.$route.params.repo}`;
+    },
+  },
+  methods: {
+    async clickMe() {
+      try {
+        await this.$api.createCollaborator({
+          owner: this.$route.params.owner,
+          repo: this.$roue.params.repo,
+          username: this.username,
+          secret: this.secret,
+        });
+        this.success = true;
+        this.failure = false;
+      } catch (e) {
+        this.failure = e.message;
+        this.success = false;
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import AddCollaborator from "./components/AddCollaborator.vue";
+import App from "./App.vue";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+import axios from "axios";
 
-Vue.config.productionTip = false
+Vue.use(Buefy);
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+
+const routes = [
+  { path: "/private-repos/:owner/:repo", component: AddCollaborator },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+/**
+ * Global variables
+ */
+Vue.prototype.$api = {
+  createCollaborator(data) {
+    return axios.post("/api/add-collaborator", data);
+  },
+};
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
